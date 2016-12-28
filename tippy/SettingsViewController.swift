fb057b6dba6344c3baa107974401fe6d2e75a9d4
Oscar Reyes <oscarreyes@Oscars-Mac.attlocal.net>
2016-12-28 11:53:17 -0800

    Updated

--- tippy/SettingsViewController.swift
+++ tippy/SettingsViewController.swift
@@ -10,6 +10,8 @@ import UIKit
 
 class SettingsViewController: UIViewController {
 
+    @IBOutlet weak var defaultTipControl: UISegmentedControl!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -21,15 +23,15 @@ class SettingsViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    @IBAction func setDefaultTip(_ sender: AnyObject) {
+        let defaults = UserDefaults.standard
+        defaults.set(defaultTipControl.selectedSegmentIndex, forKey: "default_tip_percentage")
+        defaults.synchronize()
     }
-    */
+    
+    override func viewWillAppear(_ animated: Bool) {
+        let defaults = UserDefaults.standard
+        defaultTipControl.selectedSegmentIndex = defaults.object(forKey: "default_tip_percentage") as! Int
 
+   }
 }
--- tippy/ViewController.swift
+++ tippy/ViewController.swift
@@ -21,16 +21,31 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view, typically from a nib.
     }
 
+    override func viewDidAppear(_ animated: Bool) {
+        billField.becomeFirstResponder() ;
+    }
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        let defaults = UserDefaults.standard
+        let tipValue = defaults.object(forKey: "default_tip_percentage")
+        
+        tipControl.selectedSegmentIndex = tipValue as! Int;
+        defaults.synchronize()
+    }
+    
     @IBAction func onTap(_ sender: AnyObject) {
         view.endEditing(true)
     }
    
     @IBAction func calculateTip(_ sender: AnyObject) {
+        let defaults = UserDefaults.standard
+        defaults.synchronize()
+        
         let tipPercentages = [0.18, 0.20, 0.25]
         let bill = Double(billField.text!) ?? 0
         let tip = bill * tipPercentages[tipControl.selectedSegmentIndex]
@@ -39,6 +54,7 @@ class ViewController: UIViewController {
         tipLabel.text = String(format: "$%.2f", tip)
         totalLabel.text = String(format: "$%.2f", total)
     }
+    
     
 
 }
